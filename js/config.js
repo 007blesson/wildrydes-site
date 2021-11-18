cbbdb3d1e4400829e4b2304f21f5adb55f1225e3
007blesson <93018679+007blesson@users.noreply.github.com>
2021-11-18 14:55:06 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-2_v8qpZ4E0f', // e.g. us-west-2_uXboG5pAb
-        userPoolClientId: '25ddkmj4v6hfsfvruhpfi7n4hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3ntejrjshql2gh3nsbu0jb1mlm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
